e6e372f0b6581ed442bc033d8bd180ba79d4a42d
Rémi Le Gall <126484877+Vanbecker@users.noreply.github.com>
2023-07-13 11:57:05 +0200

    instal

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(Hello World)
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+app.listen(port, () => console.log(
+    `Notre application Node est démarrée sur : http://localhost:${port}`)
+)
+
+/* 
+    Pourquoi utilise t-on require et non pas import ?
+    require() est une fonction native propre à NodeJS, 
+    elle permet de charger un module entier 
+*/
